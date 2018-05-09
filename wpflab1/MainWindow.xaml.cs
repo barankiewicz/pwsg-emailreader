dccc563c30a95ae5b86df883f874845bf7b42232
Filip Barankiewicz <barankiewicz@protonmail.ch>
2018-05-09 14:58:22 +0200

    added the project

---
+++ wpflab1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpfTask1;
+
+namespace wpflab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<EmailMessage> messages = new ObservableCollection<EmailMessage>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Opacity = 0.5;
+            Window1 wnd = new Window1();
+            wnd.ShowInTaskbar = false;
+            wnd.Owner = this;
+            wnd.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            wnd.ShowDialog();
+            if (wnd.messages != null)
+            {
+                messages = wnd.messages;
+                
+            }
+                
+            Opacity = 1;
+        }
+    }
+}
---
+++ wpflab1/Window1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using wpfTask1;
+
+namespace wpflab1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        bool isLoggedIn;
+        public ObservableCollection<EmailMessage> messages = null;
+        public Window1()
+        {
+            isLoggedIn = false;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(isLoggedIn)
+            {
+                loginBox.Text = "Login";
+
+            }
+            else
+            {
+                EmailUser loggedUser;
+                string login = loginBox.Text;
+                string password = pswdBox.Password;
+                bool success = EmailData.GetUserData(login, password, out loggedUser);
+                if (success)
+                {
+                    messages = loggedUser.MessagesReceived;
+                    isLoggedIn = true;
+                    loginBox.Text = "Logout";
+                    Close();
+                }
+                else
+                    MessageBox.Show("Login failed");
+            }
+        }
+    }
+}
